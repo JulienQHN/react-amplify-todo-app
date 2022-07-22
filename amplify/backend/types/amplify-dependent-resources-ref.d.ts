0a39d76fb008b1f25a564a931f979fae6fe4aee2
Julien <julien.quennehen@edu.devinci.fr>
2022-07-23 00:05:22 +0200

    test: 💍 testing of GraphQL playground mutations for the app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,9 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "reactamplifiedapi": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
